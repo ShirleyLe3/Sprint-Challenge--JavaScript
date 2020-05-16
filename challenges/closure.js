effb5bfe2faa79bbb130dd7519e514ed379c64f5
ShirleyLe3 <synchrotronflux@yahoo.com>
2020-05-15 20:18:22 -0400

    closures

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,22 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
+// Unlike most functions that are outside of the parent function, closures are able to rememeber the environment in which it was defined (parent funct), but accessible from outside.  This is an example of lexical scope and a very useful tool that few languages have.
 
 
 /* Task 2: Counter */
 
 /* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+function summation(num) {
+  let counter = 1;
+  return function() {
+      for(num; num > 1; num--) {
+          counter = counter + num
+      }
+      return counter;
+  }
+}
+
+const summating = summation(4);
+
+// console.log(summating());
